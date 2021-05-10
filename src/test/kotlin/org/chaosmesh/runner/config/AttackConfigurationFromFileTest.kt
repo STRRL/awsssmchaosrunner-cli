40e3c2434d258392587fc34dddf8bacc01e9ba03
STRRL <str_ruiling@outlook.com>
2021-05-10 17:52:46 +0800

    docs: fix the codes for rendering examples
    Signed-off-by: STRRL <str_ruiling@outlook.com>

--- src/test/kotlin/org/chaosmesh/runner/config/AttackConfigurationFromFileTest.kt
+++ src/test/kotlin/org/chaosmesh/runner/config/AttackConfigurationFromFileTest.kt
@@ -3,19 +3,20 @@ package org.chaosmesh.runner.config
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Test
 import software.amazon.awsssmchaosrunner.attacks.SSMAttack
+import java.time.Duration
 
 internal class AttackConfigurationFromFileTest {
     private val objectMapper = ObjectMapper()
 
     @Test
-    fun generateJson() {
+    fun generateExample() {
         val content = SSMAttack.Companion.AttackConfiguration(
-            "example-cpu-hug",
-            "20s",
+            "NetworkInterfaceLatencyAttack",
+            Duration.ofSeconds(20).toString(),
             10,
             "",
             listOf(),
-            0,
+            100,
             mapOf("networkInterfaceLatencyMs" to "")
         )
         val json = objectMapper.writeValueAsString(content)
